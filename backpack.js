d8fff5c468729c416ae2e8562906038a3e70b6da
sevemilanovic <57541306+sevemilanovic@users.noreply.github.com>
2022-03-08 10:35:56 +0100

    Update backpack.js

--- backpack.js
+++ backpack.js
@@ -11,13 +11,27 @@ function backpackItem() {
     let numberofIndex = numberOfIndex();
     return backpackAllItems[numberofIndex];
 }
-function backPack(){
-    let backpack =[];
+function ifHero (backpackitem,heroName,backpack){
+    if (heroName === 'swordsman' && backpackitem === 'sword') {
+        backpack.push(backpackitem);
+             console.log(backpackitem);
+         } else if (heroName === 'swordsman' && backpackitem === 'spear') {
+            backpack.push(backpackitem);
+            console.log(backpackitem);
+         } else if (heroName === 'wizard' && backpackitem === 'magic') {
+            backpack.push(backpackitem);
+            console.log(backpackitem);
+         } else {
+            console.log(backpackitem);
+         }
+
+}
+function backPack(heroName){
+    let backpack = [];
     console.log(backpack);
        while(backpack.length<lengthBackpack){
-        let backpackitem = backpackItem(); 
-           backpack.push(backpackitem);
-           console.log(backpackitem);
+        let backpackitem = backpackItem();
+        ifHero (backpackitem,heroName,backpack); 
 }
 return backpack;
 }
