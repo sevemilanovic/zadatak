17a63d0bf5dc824cdc7b26d54858a44aacb8e286
sevemilanovic <57541306+sevemilanovic@users.noreply.github.com>
2022-03-11 13:36:36 +0100

    Update backpack.js

--- backpack.js
+++ backpack.js
@@ -1,35 +1,46 @@
 
 
+
 let lengthBackpack = 2;
 let backpackAllItems = ['sword','spear','magic'];
 
-function numberOfIndex(){
-    let number = backpackAllItems.length;
-    return(Math.floor((Math.random()*100)% number ));
+function randomNumber(){
+    return Math.floor(Math.random()*100);
+}
+//gives random number
+function numberOfIndex(x){
+    let number = x.length;
+    return (randomNumber() % number) ;
 }; 
+//random number of array index
 
-function backpackItem() {
-    let numberofIndex = numberOfIndex();
-    return backpackAllItems[numberofIndex];
+function backpackItem(y) {
+    let numberofIndex = numberOfIndex(y);
+    return y[numberofIndex];
 }; 
+//random item in array
 
 function ifHero (backpackitem,heroName,backpack){
     if (heroName === 'swordsman' && backpackitem === 'sword') {
         backpack.push(backpackitem);
+             console.log(backpackitem);
          } else if (heroName === 'swordsman' && backpackitem === 'spear') {
             backpack.push(backpackitem);
+            console.log(backpackitem);
          } else if (heroName === 'wizard' && backpackitem === 'magic') {
             backpack.push(backpackitem);
+            console.log(backpackitem);
          }else{
             throw `${heroName} can't take ${backpackitem}`;
          }
 
 }
+//exceptions of items and heroes
 function backPack(heroName){
     let backpack = [];
     
        while(backpack.length<lengthBackpack){
-        let backpackitem = backpackItem();
+        let backpackitem = backpackItem(backpackAllItems);
         try{
             ifHero(backpackitem,heroName,backpack);
         }catch(e){
@@ -39,6 +50,11 @@ function backPack(heroName){
 }
 return backpack;
 }
+//making backpack full
+
+
+
+
 
 
 
