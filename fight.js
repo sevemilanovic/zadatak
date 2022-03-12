84dfb573887749c2681123f36a6b8be26b6944fb
sevemilanovic <57541306+sevemilanovic@users.noreply.github.com>
2022-03-12 08:53:39 +0100

    Update fight.js

--- fight.js
+++ fight.js
@@ -1,14 +1,16 @@
 function isTrue(x,y){
+    let divText = document.getElementById('divText');
     if(x<=0){
-        console.log('Hero has lost!')
+        divText.innerHTML = 'Hero has lost but monster has ' + y +' health';
         return false;
     }if(y<=0){
-        console.log('Monster is dead!')
+        divText.innerHTML = 'Monster is dead but hero has ' + x +' health';
         return false;
     }else {
         return true;
     }
 }
+//requirement for fight/while loop
 function fight(hero,monster){
             let monsterHealth = monster.health;
             let heroHealth = hero.health;
